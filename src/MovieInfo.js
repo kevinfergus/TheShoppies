4a2d789514de04cf378d20a0bfc16937cb5991ef
Kevin Fergus <kevinfergus89@gmail.com>
2020-11-17 11:37:24 -0600

    double checking to make sure save went through

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 function MovieInfo(props) {
 	useEffect(async () => {
 		const response = await axios.get(`http://www.omdbapi.com/?i=${props.selection.imdbID}&apikey=52140488`);
-		props.setSelection(response.data);
 	});
 
 	return <div> Hello </div>;
