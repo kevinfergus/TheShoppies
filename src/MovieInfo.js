fa95ca46515bf82b04017be498e5a8491184677b
Kevin Fergus <kevinfergus89@gmail.com>
2020-11-19 11:50:21 -0600

    firestore set up working on incrementing rating count

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,5 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
+import {db}from './firebase'
+require("firebase/firestore");
+
 
 function MovieInfo(props) {
 
@@ -13,8 +16,65 @@ function MovieInfo(props) {
 		fetchMovieInfo()
 	}, []);
 
-	const handleClick = ()=>  {
+	const handleBackClick = ()=>  {
 		props.setSelection(false)
+	}
+
+	const handleThumbClick =(e) => {
+		const title= props.selection.Title
+		const movieDocRef = db.collection("ratings").doc(title);
+		const rating = e.target.value
+	
+		var sfDocRef = movieDocRef
+
+
+sfDocRef.set({ population: sfDocRef.population });
+
+return db.runTransaction(function(transaction) {
+    // This code may get re-run multiple times if there are conflicts.
+    return transaction.get(sfDocRef).then(function(sfDoc) {
+        if (!sfDoc.exists) {
+            throw "Document does not exist!";
+        }
+
+        // Add one person to the city population.
+        // Note: this could be done without a transaction
+        //       by updating the population using FieldValue.increment()
+        var newPopulation = sfDoc.data().population + 1;
+        transaction.update(sfDocRef, { population: newPopulation });
+    });
+}).then(function() {
+    console.log("Transaction successfully committed!");
+}).catch(function(error) {
+    console.log("Transaction failed: ", error);
+});
+
+ 
+
+		// return db.runTransaction(function(transaction) {
+  
+    	// 	return transaction.get(movieDocRef).then(function(movieDoc) {
+       	// 		 if (!movieDoc.exists) {
+        //    			 throw "Document does not exist!";
+       	// 		 }
+
+        
+		// 			const newRating = movieDoc.data().thumbsUP + 1
+		// 			console.log('rating', rating)
+		// 			console.log('newRating', newRating)
+		// 			if(rating==='thumbsUP') {
+		// 					transaction.update(movieDocRef, { thumbsUP: newRating });
+		// 			}
+		// 			if(rating==="thumbsDOWN") {
+		// 				transaction.update(movieDocRef, { thumbsEVEN: newRating });
+		// 			}
+    	// 	});
+		// }).then(function() {
+   		//  console.log("Transaction successfully committed!");
+		// }).catch(function(error) {
+   		//  console.log("Transaction failed: ", error);
+		// });
+
 	}
 	return (
 		<div>
@@ -32,9 +92,9 @@ function MovieInfo(props) {
 					<td>{props.selection.Year}</td>
   				</tr>
   			</table>
-			<button type="button" onClick={()=>handleClick()}>Back</button>
-			<button type ="button" value="thumbs up">Thumbs Up</button>
-			<button type ="button" value="thumbs down">Thumbs Down</button>
+			<button type="button" onClick={()=>handleBackClick()}>Back</button>
+			<button type ="button" value="thumbsUP" onClick={(e)=>handleThumbClick(e)}>Thumbs Up</button>
+			<button type ="button" value="thumbsDOWN" onClick={(e)=>handleThumbClick(e)}>Thumbs Down</button>
 
 		</div>
 	)
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+const config = {
+    apiKey: "AIzaSyBUKU9LrYl27C4fD4SwdVFbUFm7_B6rDXo",
+    authDomain: "yearone-api-design.firebaseapp.com",
+    databaseURL: "https://yearone-api-design.firebaseio.com",
+    projectId: "yearone-api-design",
+    storageBucket: "yearone-api-design.appspot.com",
+    messagingSenderId: "664680206962",
+    appId: "1:664680206962:web:a61e2c7facabc98862a8eb",
+    measurementId: "G-54HLH7JWYB"
+  };
+  
+
+export const fire = firebase.initializeApp(config);
+
+export const db = fire.firestore();
