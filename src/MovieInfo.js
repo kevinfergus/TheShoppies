e613d6475e1e58a7e2d37d04020c96b923cac144
Kevin Fergus <kevinfergus89@gmail.com>
2020-11-20 11:07:00 -0600

    doc is created or updated in movie info as appropriate

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -20,58 +20,80 @@ function MovieInfo(props) {
 	const handleBackClick = ()=>  {
 		props.setSelection(false)
 	}
+	const createDoc = (rating, movieDocRef) => {
+		if(rating==="thumbsUP") 
+					{
+						 movieDocRef.set({thumbsUP: 1, thumbsDOWN: 0}).then(function() {
+							console.log("Document successfully written!");
+							return 'Doc created'
+						})
+						.catch(function(error) {
+							console.error("Error writing document: ", error);
+						});
+					}
+					if (rating ==="thumbsDOWN") {
+						movieDocRef.set({thumbsUP: 0, thumbsDOWN: 1} ).then(function() {
+						   console.log("Document successfully written!");
+						   return 'Doc created'
+					   }).catch(function(error) {
+						   console.error("Error writing document: ", error);
+					   });
+				   }
+	}
 
-	const  handleThumbClick =async(e) => {
+	const updateDoc = (rating, movieDocRef) => {
+		db.runTransaction(function(transaction) {
+			return transaction.get(movieDocRef).then(function(movieDoc) {
+
+			  if(movieDoc.exists) {
+				  let newRating
+					  if(rating==='thumbsUP') {
+						  newRating = movieDoc.data().thumbsUP + 1	
+
+						  transaction.update(movieDocRef, { thumbsUP: newRating });
+						  return newRating
+					  }
+					  if(rating==="thumbsDOWN") {
+						  newRating = movieDoc.data().thumbsDOWN + 1	
+
+						  transaction.update(movieDocRef, { thumbsDOWN: newRating });
+						  return newRating
+					  }
+			  }
+			  else {
+			  }
+
+		  });
+	  }).then(function() {
+		  console.log("Transaction successfully committed!");
+	  }).catch(function(error) {
+		  console.log("Transaction failed: ", error);
+	  })
+	}
+
+	const  handleThumbClick =(e) => {
 		const title= props.selection.Title
 		const movieDocRef = db.collection("ratings").doc(title);
 		const rating = e.target.value
-
-		///wrap the set call in a function that you can call if it doesn't exist
-
-
-
-
-		
-		db.runTransaction(function(transaction) {
-  
-    		return transaction.get(movieDocRef).then(function(movieDoc) {
-       			 if (!movieDoc.exists) {
-					if(rating==="thumbsUP") 
-						{
-							movieDocRef.set({thumbsUP: 1}).then(function() {
-								console.log("Document successfully written!");
-							})
-							.catch(function(error) {
-								console.error("Error writing document: ", error);
-							});
-						}
-					if (rating ==="thumbsDOWN") {
-						movieDocRef.set({thumbsDOWN: 1})
-					}
-       			 }
-
-					else {
-					const newRating = movieDoc.data().thumbsUP + 1
-					console.log('new rating', newRating)
-	
-					if(rating==='thumbsUP') {
-							transaction.update(movieDocRef, { thumbsUP: newRating });
-					}
-					if(rating==="thumbsDOWN") {
-						transaction.update(movieDocRef, { thumbsDOWN: newRating });
-					}
-				}
-    		});
-		}).then(function() {
-   		 console.log("Transaction successfully committed!");
+		let docExists 
+		movieDocRef.get().then(function(doc) {
+			if (doc.exists) {
+				docExists=true
+			} else {
+				// doc.data() will be undefined in this case
+				docExists=false
+			}
 		}).catch(function(error) {
-   		 console.log("Transaction failed: ", error);
+			console.log("Error getting document:", error);
 		});
-		
-
 	
 	
-
+		if(!docExists) {
+			createDoc(rating, movieDocRef)
+		}
+		else {
+			updateDoc(rating, movieDocRef)
+		}
 
 	}
 	return (
